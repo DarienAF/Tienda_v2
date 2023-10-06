868bec1db54a78c83a5cd6f7404f8ae43136200a
DarienAF <jafdarien@gmail.com>
2023-10-06 16:33:36 -0600

    Semana 04

---
+++ src/test/java/com/tienda_v2/ProjectConfig.java
@@ -0,0 +1,50 @@
+package com.tienda_v2;
+
+import java.util.Locale;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+
+public class ProjectConfig implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localResolver() {
+        var slr = new SessionLocaleResolver();
+
+        slr.setDefaultLocale(Locale.getDefault());
+        slr.setLocaleAttributeName("session.current.locale");
+        slr.setTimeZoneAttributeName("session.current.locale");
+
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        var lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registro) {
+        registro.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean("messageSource")
+    public MessageSource messageSourge() {
+        ResourceBundleMessageSource bundleMessageSource
+                = new ResourceBundleMessageSource();
+        bundleMessageSource.setBasename("message");
+        bundleMessageSource.setDefaultEncoding("UTF-8");
+
+        return bundleMessageSource;
+    }
+}
