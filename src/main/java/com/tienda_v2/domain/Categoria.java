05825a5d98268fa0efd0f8536d5e71ca8bee0e46
DarienAF <jafdarien@gmail.com>
2023-10-20 21:36:44 -0600

    Semana6

--- src/main/java/com/tienda_v2/domain/Categoria.java
+++ src/main/java/com/tienda_v2/domain/Categoria.java
@@ -7,31 +7,26 @@ import lombok.Data;
 @Data
 @Entity
 @Table(name="categoria")
-public class Categoria implements Serializable {
+public class Categoria implements Serializable{
     
-    private static final long serialVersionUID = 1L;
+    private static final Long serialVersionUID= 1L;
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id_categoria")
+    @Column (name="id_categoria")
     private Long idCategoria;
     
     private String descripcion;
     private String rutaImagen;
-    private boolean activo;
+    private Boolean activo;
 
     public Categoria() {
     }
-    
-    public Categoria(String descripcion, boolean activo) {
+
+    public Categoria(String descripcion, Boolean activo) {
         this.descripcion = descripcion;
         this.activo = activo;
     }
-
     
     
-    
-    
-    
-
 }
