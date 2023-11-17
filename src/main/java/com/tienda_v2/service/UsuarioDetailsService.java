2c7acfdd56202e2866925ba47ee851cbd9b9d5e7
DarienAF <jafdarien@gmail.com>
2023-11-17 17:18:35 -0600

    Semana 10

---
+++ src/main/java/com/tienda_v2/service/UsuarioDetailsService.java
@@ -0,0 +1,10 @@
+
+package com.tienda_v2.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public interface UsuarioDetailsService {
+    public UserDetails loadUserByUsername(String username) 
+           throws UsernameNotFoundException;
+}
